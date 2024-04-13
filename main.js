64d3bfea1ac5acbe8891d0af3ac631f4ae1307d9
Nihil2000 <151999444+Nihil2000@users.noreply.github.com>
2024-04-13 16:48:28 +0900

    footer

--- main.js
+++ main.js
@@ -8,3 +8,13 @@ function header(rootDir){
         }
     });
 }
+function footer(rootDir){
+    $.ajax({
+        url: rootDir + "footer.html",
+        cache: false,
+        success: function(html){
+            html = html.replace(/\{\$root\}/g, rootDir);
+            document.getElementById('footer').innerHTML = html;
+        }
+    });
+}
